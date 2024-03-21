394b851931ab132a39df887242293b10315dd3eb
Ekaterina Koshkina <ekaterinakoshkina@mbp-ekaterina.mynetworksettings.com>
2024-03-21 14:31:29 -0400

    second

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-let body = document.querySelector("body");
-
-const usePrompt = () => {
-  let userInput = prompt(" Insert a question if you are not scared to know your destiny ");
+let h2 = document.querySelector("h2");
+const usePromptOne = () => {
+  let userInput = prompt("Insert a question if you are not scared to know your destiny");
   console.log(userInput);
-  body.append(userInput);
+  h2.append(userInput);
+  setTimeout(function() {document.getElementById('question').innerHTML='';},5000);
+  document.getElementById("question").innerText=random;
 };
-
 function GetAnswer()
 {
     let myarray= new Array("Yes","No","Maybe","Try again Later","It is certain","It is decidedly so","Without a doubt","Most likely","Reply hazy,try again","Don't count on it","My sources say no","Outlook not so good","Very doubtful");
     let random = myarray[Math.floor(Math.random() * myarray.length)];
-    // alert(random);
+    setTimeout(function() {document.getElementById('answer').innerHTML='';},3000)
     document.getElementById("answer").innerText=random;
 }
 
